15db5a8c0544c868bc01f3c8c7ce45bb2cd724c9
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-09-26 15:53:08 -0700

    Added function  that allowed for external configuration of the url builder.

--- app/code/local/CarlosReynosa/SpotifyPlayButton/Model/PlayButtonConfig.php
+++ app/code/local/CarlosReynosa/SpotifyPlayButton/Model/PlayButtonConfig.php
@@ -1,23 +1,38 @@
 <?php
-
 /**
- * Created by PhpStorm.
- * User: carlos
- * Date: 8/1/15
- * Time: 2:30 PM
+ * Class used to store and build widget configuration for final rendering.
  */
 class CarlosReynosa_SpotifyPlayButton_Model_PlayButtonConfig extends Mage_Core_Model_Abstract
 {
 
+    private $widgetUrl;
+
+    /**
+     * Set defaults for constructing widget parameters that allow proper rendering.
+     */
     protected function _construct()
     {
-        $urlBuilder = Mage::getModel('CarlosReynosa_SpotifyPlayButton/PlayButtonUrl');
-        $url = $urlBuilder->getUrl();
+        $this->widgetUrl = Mage::getModel('CarlosReynosa_SpotifyPlayButton/PlayButtonUrl');
         $this->setWidth(300);
-        $this->setHeight(80);
-        $this->setUrl($url);
-
-
-        $this->setUri('spotify:album:57bABnvvPfNhBQRI70dqlU');
+        $this->setHeight(380);
     }
+
+    /**
+     * Sets the URI used to build the final widget URL.
+     * @param $id
+     */
+    public function setUriId($id)
+    {
+        $this->widgetUrl->setUriId($id);
+    }
+
+    /**
+     * Returns the final widget URL used to render the widget.
+     * @return String
+     */
+    public function getUrl()
+    {
+        return $this->widgetUrl->getUrl();
+    }
+
 }
