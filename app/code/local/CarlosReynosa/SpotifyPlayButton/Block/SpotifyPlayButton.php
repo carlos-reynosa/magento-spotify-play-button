98bf79f94271f6007e0697350dfa077bceb5a705
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-09-26 09:51:57 -0700

    Added constructor for setting the rendering context config of the spotify play button.

--- app/code/local/CarlosReynosa/SpotifyPlayButton/Block/SpotifyPlayButton.php
+++ app/code/local/CarlosReynosa/SpotifyPlayButton/Block/SpotifyPlayButton.php
@@ -3,4 +3,13 @@
 class CarlosReynosa_SpotifyPlayButton_Block_SpotifyPlayButton extends Mage_Core_Block_Template
 {
 
+    public function _construct()
+    {
+        $config = Mage::getModel('CarlosReynosa_SpotifyPlayButton/PlayButtonConfig');
+
+        $this->setPlayButtonUrl($config->getUrl());
+        $this->setWidth($config->getWidth());
+        $this->setHeight($config->getHeight());
+    }
+
 }
