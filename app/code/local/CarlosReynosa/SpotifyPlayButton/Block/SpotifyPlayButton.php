5ff2cf5441fcb669275bdf56833cd52a9bb5ba19
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-09-26 15:57:13 -0700

    Added processing for widget parameters  attained  from shorthand codes to override defaults.

--- app/code/local/CarlosReynosa/SpotifyPlayButton/Block/SpotifyPlayButton.php
+++ app/code/local/CarlosReynosa/SpotifyPlayButton/Block/SpotifyPlayButton.php
@@ -3,13 +3,47 @@
 class CarlosReynosa_SpotifyPlayButton_Block_SpotifyPlayButton extends Mage_Core_Block_Template
 {
 
+    private $config;
+
     public function _construct()
     {
+
+        $this->config = $this->getFinalConfig();
+        $this->setWidth($this->config->getWidth());
+        $this->setHeight($this->config->getHeight());
+    }
+
+    private function getFinalConfig()
+    {
+        //Config with defaults
         $config = Mage::getModel('CarlosReynosa_SpotifyPlayButton/PlayButtonConfig');
 
-        $this->setPlayButtonUrl($config->getUrl());
-        $this->setWidth($config->getWidth());
-        $this->setHeight($config->getHeight());
+        //Config values from other sources override defaults
+        $uriId = $this->getUriId();
+
+        if (isset($uriId) && $uriId != '') {
+            $config->setUriId($this->getUriId());
+        }
+
+        $width = $this->getWidth();
+
+        if (isset($width) && $width != '') {
+            $config->setWidth($width);
+        }
+
+        $height = $this->getHeight();
+
+        if (isset($height) && $height != '') {
+            $config->setHeight($height);
+        }
+
+        return $config;
+    }
+
+    public function getWidgetUrl()
+    {
+
+        return $this->config->getUrl();
     }
 
 }
